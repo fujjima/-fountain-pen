13744ac2822105f40ae087c423a1f6b779f86af5
katsuimasato <masato.lond2@outlook.jp>
2020-09-08 19:40:31 +0900

    get data by scraping

--- app/controllers/fortain_pens_controller.rb
+++ app/controllers/fortain_pens_controller.rb
@@ -1,9 +1,32 @@
+# frozen_string_literal: true
+
+# FIXME: スクレイピング機能はモジュールに移す
+require 'nokogiri'
+require 'open-uri'
+
 class FortainPensController < ApplicationController
-  def index; end
+  before_action :scraping, only: [:index]
+  # スクレイピング結果一覧を表示
+  def index
+    @test
+  end
 
   def show; end
 
   def destroy; end
 
   def update; end
+
+  def scraping
+    url = 'https://www.pilot.co.jp/products/pen/fountain/fountain/custom_urushi/'
+    charset = nil
+    html = open(url) do |h|
+      charset = h.charset
+      h.read
+    end
+    doc = Nokogiri::HTML.parse(html)
+    doc.xpath('//table[@class="dataTableA01"]').each do |node|
+      @test ||= node.css('p').inner_text
+    end
+  end
 end
