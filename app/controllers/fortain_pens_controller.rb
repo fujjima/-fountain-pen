db38d03a8139a140f9a8b62d4d50ababd9a3df32
katsuimasato <masato.lond2@outlook.jp>
2020-09-22 18:32:08 +0900

    get fortain pen name from title when datatable do not has name

--- app/controllers/fortain_pens_controller.rb
+++ app/controllers/fortain_pens_controller.rb
@@ -6,15 +6,21 @@ require 'open-uri'
 
 FORTAIN_PEN_URL = 'https://www.pilot.co.jp/products/pen/fountain/'
 
+# FIXME: カスタム742だけテーブルに製品名がないため別対応を施しているが、xpath内で対応できるなら修正したい
+CUSTOM_742_URL = 'https://www.pilot.co.jp/products/pen/fountain/fountain/custom742/'
+CUSTOM_HERITAGE_912_URL = 'https://www.pilot.co.jp/products/pen/fountain/fountain/custom_heritage912/'
+
 # たまに万年筆以外のものが混じっている可能性がある（ボトルインキなど）
 TARGET_TABLE_PATH = "//table[@class='dataTableA01' and .//th[text()='サイズ']]"
 
-# サイトの'ペン'の表記が一部おかしいので、複数パターン書いている
+# サイトの'ペン'の表記ゆれがあるので、複数パターンを書いている
+# カスタム742は製品名がないので、//span[@class='titleLabel'][1]
 TARGET_DATA_PATH = "//th[text()='製品名' or text()='価格' or text()='ペン種' or text()='ぺン種']//following-sibling::td"
+
 
 class FortainPensController < ApplicationController
   before_action :scraping, only: [:index]
-  # スクレイピング結果一覧を表示
+
   def index
     @fortain_pen_datasdatas
   end
@@ -43,7 +49,12 @@ class FortainPensController < ApplicationController
     urls.each do |url|
       html = open(url, &:read)
       doc = Nokogiri::HTML.parse(html)
-      # doc.xpath("//th[text()='製品名' or text()='価格' or text()='ペン種' or text()='ぺン種']/following-sibling::td").each do |node| end
+
+      # カスタム742、カスタムヘリテイジ912に製品名がない
+      if [CUSTOM_742_URL, CUSTOM_HERITAGE_912_URL].include? url
+        data << doc.xpath("//span[@class='titleLabel']")[0]&.inner_text
+      end
+      
       doc.xpath(TARGET_TABLE_PATH + TARGET_DATA_PATH).each do |node|
         data << node.css('p').inner_text
       end
@@ -54,10 +65,10 @@ class FortainPensController < ApplicationController
     # modify_kind!
   end
 
-  # 金額整形
+  # 金額を整形した状態の配列を返す
   def modify_price!
-    @fortain_pen_datas.map! do |d|
-      d[1].match('円').pre_match.delete(',').to_i
+    @fortain_pen_datas.map do |d|
+      d[1] = d[1].match('円').pre_match.delete(',').to_i
     end
   end
 end
